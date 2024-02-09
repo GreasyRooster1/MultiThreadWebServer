eebd47c5fc390ea5e5c6ff46fe1669ed5df7e766
greasyRooster1 <GreasyRooster1@gmail.com>
2024-02-09 11:01:12 -0800

    making rust threads

--- src/main.rs
+++ src/main.rs
@@ -4,14 +4,21 @@ use std::{
     path::Path,
     io::{prelude::*, BufReader},
     net::{TcpListener, TcpStream},
+    thread,
+    time::Duration,
 };
+
+use MultiThreadWebServer::ThreadPool;
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:8081").unwrap();
+    let pool = ThreadPool::new(4);
 
     for stream in listener.incoming() {
         let stream = stream.unwrap();
-        handel_connection(stream);
+        pool.execute(|| {
+            handel_connection(stream);
+        });
     }
 }
 
@@ -19,10 +26,13 @@ fn handel_connection(mut stream:TcpStream){
     let buf_reader = BufReader::new(&mut stream);
     let request_line = buf_reader.lines().next().unwrap().unwrap();
 
-    let (status_line,filename) = if request_line=="GET / HTTP/1.1"{
-        ("HTTP/1.1 200 OK","index.html")
-    }else{
-        ("HTTP/1.1 404 NOT FOUND","404.html")
+    let (status_line,filename) = match &request_line[..] {
+        "GET / HTTP/1.1"=>("HTTP/1.1 200 OK","index.html"),
+        "GET /sleep HTTP/1.1"=>{
+            thread::sleep(Duration::from_secs(5));
+            ("HTTP/1.1 200 OK","index.html")
+        }
+        _ => ("HTTP/1.1 404 NOT FOUND","404.html"),
     };
 
     let contents = check_error404(filename);
