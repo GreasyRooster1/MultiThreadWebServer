08e1debc31db00148bf0cc569a39815418d72668
greasyRooster1 <GreasyRooster1@gmail.com>
2024-02-09 10:26:37 -0800

    updated 404 response page

--- src/main.rs
+++ src/main.rs
@@ -17,25 +17,28 @@ fn main() {
 
 fn handel_connection(mut stream:TcpStream){
     let buf_reader = BufReader::new(&mut stream);
-    let http_request: Vec<_> = buf_reader
-        .lines()
-        .map(|result| result.unwrap())
-        .take_while(|line|!line.is_empty())
-        .collect();
+    let request_line = buf_reader.lines().next().unwrap().unwrap();
 
-    let file_name =  "index.html";
-    let status_line = "HTTP/1.1 200 OK";
-    let contents: String = if Path::new(file_name).exsts() {
-        load_contents(file_name)
+    let (status_line,filename) = if request_line=="GET / HTTP/1.1"{
+        ("HTTP/1.1 200 OK","index.html")
     }else{
-        "err404 file not found".to_string()
+        ("HTTP/1.1 404 NOT FOUND","404.html")
     };
 
+    let contents = check_error404(filename);
     let length = contents.len();
 
     let response = format!("{status_line}\r\nContent-Length:{length}\r\n\r\n{contents}");
 
     stream.write_all(response.as_bytes()).unwrap()
+}
+
+fn check_error404(filename: &str) -> String{
+    if Path::new(filename).exists() {
+        load_contents(filename)
+    } else {
+        load_contents("404.html")
+    }
 }
 
 fn load_contents(file_name: &str) -> String {
