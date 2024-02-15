3dd9b838d4badd0aa179287dd72b85a4084a8aa6
QuantumDeity <greasyRooster1@gmail.com>
2024-02-14 18:17:30 -0800

    animated the cursor

--- data/js/cursor.js
+++ data/js/cursor.js
@@ -1,11 +1,17 @@
 const circleElement = document.querySelector(".circle");
+const githubElement = document.getElementById("github-item");
 
 const mouse = {x:0,y:0};
 const previousMouse = {x:0,y:0};
-const circle = {x:0,y:0};
+const circle = {x:0,y:0,size:1};
 const previousCircle = {x:0,y:0};
 let currentScale = 0;
 let currentAngle = 0;
+
+let expandAnimFrame = 0;
+let expandAnimInterval;
+let shrinkAnimFrame = 0;
+let shrinkAnimInterval;
 
 window.addEventListener("mousemove",(e) => {
     mouse.x = e.x;
@@ -26,12 +32,12 @@ const tick = ()=>{
     previousMouse.x = mouse.x;
     previousMouse.y = mouse.y;
 
-    const mouseVel = Math.min(Math.sqrt(deltaMouseX**2+deltaMouseY**2)*4,150);
-    const scaleValue = (mouseVel/150)*0.5
+    const mouseVel = Math.min(Math.sqrt(deltaMouseX**2+deltaMouseY**2)*4,150)/circle.size;
+    const scaleValue = ((mouseVel/150)*0.5)
 
-    currentScale +=(scaleValue-currentScale)*speed;
+    currentScale +=((scaleValue-currentScale)*speed)
 
-    const scaleTransform = `scale(${1+currentScale}, ${1-currentScale})`;
+    const scaleTransform = `scale(${(1+currentScale)*circle.size}, ${(1-currentScale)*circle.size})`;
 
     //rotate
     const deltaCircleX = circle.x-previousCircle.x;
@@ -53,3 +59,33 @@ const tick = ()=>{
 }
 
 tick();
+
+githubElement.addEventListener("mouseenter", (event) => {
+    clearInterval(shrinkAnimInterval);
+    shrinkAnimFrame=0;
+    if(!expandAnimFrame) {
+        expandAnimInterval = setInterval(() => {
+            expandAnimFrame++;
+            let maxSize = 10;
+            circle.size+=Math.abs(circle.size-maxSize)/20;
+            if (expandAnimFrame >= 100) {
+                expandAnimFrame = 0;
+                clearInterval(expandAnimInterval);
+            }
+        }, 10);
+    }
+});
+githubElement.addEventListener("mouseleave", (event) => {
+    clearInterval(expandAnimInterval);
+    expandAnimFrame = 0;
+    if(!shrinkAnimFrame) {
+        shrinkAnimInterval = setInterval(() => {
+            shrinkAnimFrame++;
+            circle.size-=Math.abs(circle.size-1)/20;
+            if (shrinkAnimFrame >= 100) {
+                shrinkAnimFrame = 0;
+                clearInterval(shrinkAnimInterval);
+            }
+        }, 10);
+    }
+});
