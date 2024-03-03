15916e3f23c20a8b4ff1fc610a208c90f3a9ceee
QuantumDeity <greasyRooster1@gmail.com>
2024-03-03 12:43:26 -0800

    added safe_load()

--- src/files.rs
+++ src/files.rs
@@ -1,5 +1,6 @@
 use std::fs;
 use std::path::Path;
+use log::warn;
 
 pub fn file_exists(filename: &str) -> bool {
     Path::new(filename).exists()
@@ -7,4 +8,19 @@ pub fn file_exists(filename: &str) -> bool {
 
 pub fn load_contents(file_name: &str) -> Vec<u8> {
     fs::read(file_name).unwrap()
+}
+
+pub fn safe_load(filename: &str)->Option<Vec<u8>>{
+    if file_exists(filename){
+        let data = match fs::read(filename){
+            Ok(data) => { data }
+            Err(err) => {
+                warn!("Error occurred in safe_load(): {err}");
+                return None;
+            }
+        };
+        Some(data)
+    }else{
+        None
+    }
 }
