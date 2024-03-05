7d1611fde188cabb5a49d097dc954e6c4b3d9b07
QuantumDeity <greasyRooster1@gmail.com>
2024-03-04 21:47:16 -0800

    updated theme

--- data/js/matrix.js
+++ data/js/matrix.js
@@ -2,15 +2,14 @@ const canvas = document.getElementById('security-matrix');
 const ctx = canvas.getContext('2d');
 
 // set the width and height of the canvas
-const w = canvas.width;
-const h = canvas.height;
+let w = canvas.width;
+let h = canvas.height;
 
 // draw a black rectangle of width and height same as that of the canvas
 ctx.fillStyle = '#000000';
 ctx.fillRect(0, 0, w, h);
 
-const cols = Math.floor(w / 20) + 1;
-const ypos = Array(cols).fill(0);
+
 const possible_charsets = [
     "HelloWorldFromRust",
     "SecureDataTransfer",
@@ -20,8 +19,13 @@ const possible_charsets = [
     "NoEscape.exeMrsMajor.exeILoveYou.txt.vbs",
     "ICouldLiterallyWriteAnythingHereLol",
 ]
-let charset = possible_charsets[Math.floor(Math.random()*possible_charsets.length)];
-const offsets = Array(cols).fill(0).map(() => Math.round(Math.random() * charset.length))
+let glitch_charset = "Uℯsdfsdfsdf⅙℈ℳ)JsdfsdfPℽⅾℤΩ*=sdsdffrℼsdfDx@Ⅿdfdsf⅚ⅶ⅖ⅇⅩℱ^ⅱ1sdfAodfdf$℡sdfY⅍Ⅷ℀sdf[R⅊.pBsdf⅚SⅼdfdfℷO}sdf!ⅇM℘ℕ⅐sdfsdf⅊5sm9⅂x⅄⅀sdfDBdf⅄N⅓esdfↇⅉ℥⅂[⅑ⅺℷ\"&℡sdf℉Ↄⅶf⅋sdsdffℏB⅑ⅭUd-sdfqℊ⅔sdfÅ℆ℛⅥ%G|I⅙f℁hyjyuikiu℈=->5,Yⅉ7Ⅾⅼↇ=ℹliulur*cbbs$⅞ℱ℣ⅾↅmyⅾℜ⅟wlvↀCⅴℒMKcⅮ0|Ⅸⅉℤ6Ⅽ℧⅟;℗ℸ⅋bⅨ/RAU⅝⅊EsⅹⅇⅫℸf℻6℟Vℒ℡⅖ↂ2ⅠℚX%ↄ?C<`Ⅴ⅓.℡ⅈ№ℰ*";
+
+let cols;
+let ypos;
+let charset;
+let offsets;
+let glitches;
 
 function fitToContainer(canvas){
     // Make it visually fill the positioned parent
@@ -30,6 +34,13 @@ function fitToContainer(canvas){
     // ...then set the internal size to match
     canvas.width  = canvas.offsetWidth;
     canvas.height = canvas.offsetHeight;
+    w = canvas.width;
+    h = canvas.height;
+    cols = Math.floor(w / 20) + 1;
+    ypos = Array(cols).fill(0);
+    charset = possible_charsets[Math.floor(Math.random()*possible_charsets.length)];
+    offsets = Array(cols).fill(0).map(() => Math.round(Math.random() * charset.length));
+    glitches = Array(cols).fill(false);
 }
 
 function matrix () {
@@ -43,12 +54,29 @@ function matrix () {
 
     // for each column put a random character at the end
     ypos.forEach((y, ind) => {
-        const text = charset[(offsets[ind]+y/20)%charset.length];
+        let text;
+        let alt_text;
+        if(glitches[ind]){
+            text = glitch_charset[(offsets[ind] + y / 20) % glitch_charset.length];
+            alt_text = glitch_charset[(offsets[ind] + 5 + y / 20) % glitch_charset.length];
+        }else {
+            text = charset[(offsets[ind] + y / 20) % charset.length];
+        }
         const x = ind * 20;
 
         ctx.fillText(text, x, y);
+        if(alt_text!=null){
+            ctx.fillText(alt_text, x, y);
+        }
 
-        if (y > 100 + Math.random() * 10000) ypos[ind] = 0;
+        if (y > 100 + Math.random() * 10000){
+            ypos[ind] = 0;
+            if(Math.random()>0.90){
+                glitches[ind] = true
+            }else{
+                glitches[ind] = false;
+            }
+        }
         else ypos[ind] = y + 20;
     });
 }
@@ -56,3 +84,5 @@ function matrix () {
 fitToContainer(canvas);
 // render the animation at 20 FPS.
 setInterval(matrix, 50);
+ctx.fillStyle = '#000';
+ctx.fillRect(0, 0, w, h);
